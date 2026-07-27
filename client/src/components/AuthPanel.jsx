4d820aa98a1b497f123d3da96a70a3e54d986f0f
Sandra CHAMP-RIGOT <sandra.champrigot@gmail.com>
2026-07-27 21:07:52 +0200

    fix: correction de l'import du logo d'authentification

--- client/src/components/AuthPanel.jsx
+++ client/src/components/AuthPanel.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { CheckCircle, Eye, EyeClosed, XCircle } from '@phosphor-icons/react';
-import urbanflowSymbol from '../assets/brand/urbanflow-Symbol.svg';
+import urbanflowSymbol from '../assets/brand/urbanflow-symbol.svg';
 import './AuthPanel.css';
 
 const PASSWORD_RULES = [
